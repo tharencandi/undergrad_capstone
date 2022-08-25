a6cfa1bf374b882bf0a922519b6973173ac3d8f9
H <haeata.ash@pm.me>
2022-08-25 12:27:45 +1000

    Added cml error message to script. Added progress file to log prior downloads

--- src/download/gdc_client.py
+++ src/download/gdc_client.py
@@ -2,31 +2,33 @@ import requests, json, logging
 from requests import HTTPError
 from integrity import *
 
-
 class gdc_client:
     def __init__(self):
         self.data_url = "https://api.gdc.cancer.gov/data"
-
-    def download_file(self, uuid: str, md5sum: str, filename: str):
-        param = json.dumps({"ids": uuid})
-
+    
+    def download_file(self, uuid: str, md5sum: str,  filename: str):
+        param = json.dumps({
+            "ids": uuid
+        })
+        
         logging.info("Beginning download for uuid: %s, filename %s", uuid, filename)
         res = requests.post(
-            self.data_url, data=param, headers={"Content-Type": "application/json"}
-        )
+            self.data_url, 
+            data=param, 
+            headers={
+                "Content-Type": "application/json"   
+            })
         try:
             res.raise_for_status()
-            is_valid_res = byte_obj_checksum(res.content, md5sum)
+            is_valid_res=byte_obj_checksum(res.content, md5sum)
             if not is_valid_res:
                 raise DownloadError(reason="Response content did not match checksum.")
             with open(filename, "wb") as f:
                 f.write(res.content)
-
-            verified_write = file_checksum(filename, md5sum)
+            
+            verified_write=file_checksum(filename, md5sum)
             if not verified_write:
-                raise DownloadError(
-                    reason="Data written to file did not match checksum"
-                )
+                raise DownloadError(reason="Data written to file did not match checksum")
             logging.info("Download finished for uuid %s, filename %s", uuid, filename)
         except HTTPError:
             logging.exception("HTTP request failed")
--- src/download_script.py
+++ src/download_script.py
@@ -4,6 +4,7 @@ from download.gdc_client import gdc_client
 from download.DownloadError import DownloadError
 
 logging.basicConfig(level=logging.DEBUG)
+
 DELIM = "\t"
 I_ID = 0
 I_FILENAME = 1
@@ -15,14 +16,17 @@ SVS_EXTENSION = "svs"
 TGCA_PREFIX = "TGCA"
 GBM = "GBM"
 LGG = "LGG"
-OUT_DIR_PATH = "downloaded_data"
-PROGRESS_STATUS = "progress.txt"
+OUT_DIR_PATH = "../downloaded_data"
+PROGRESS_FILE_PATH= "../progress.txt"
 
-
+I_CML_MANIFEST=1
+CML_MSG="""
+Usage:\n
+python3 download_script.py [manifest file path]
+"""
 def has_file_ext(fname, ext):
-    r = re.compile("[\S]*\." + ext + "$")
+    r = re.compile("[\S]+\." + ext + "$")
     return r.match(fname)
-
 
 def create_output_dir(path):
     if not os.path.exists(path):
@@ -32,25 +36,44 @@ def create_output_dir(path):
         print("Can't create output_dir", file=sys.stderr)
         exit(os.EX_CANTCREAT)
 
+def create_progress_file(path):
+    if os.path.exists(path) and os.path.isdir(path):
+        logging.critical("Cannot create progress file. Path already exists and it is a directory.")
+        print("Can't create progress file.", file=sys.stderr)
+        exit(os.EX_CANTCREAT)
+    elif not os.path.exists(path):
+        with open(path, "a") as f:
+            return
+
+
+def cml():
+    if len(sys.argv) < 2:
+        print(CML_MSG, file=sys.stderr)
+        exit(os.EX_USAGE)
 
 if __name__ == "__main__":
+    cml()
     create_output_dir(OUT_DIR_PATH)
+    create_progress_file(PROGRESS_FILE_PATH)
     gdc = gdc_client()
-    with open(sys.argv[1], "r") as f:
-        for ln in f:
-            ln = f.readline().strip()
-            logging.info("Manifest line read: %s", ln)
-            sln = ln.split(DELIM)
-            if len(ln) < 3:
-                logging.warning(
-                    "Unexpected data format: Split line has length: %s, expected length of 4. Line was: %s",
-                    len(sln),
-                    ln,
-                )
-                continue
+    with open(sys.argv[I_CML_MANIFEST], "r") as f:
+        with open(PROGRESS_FILE_PATH, "a") as p:
+            for ln in f:
+                ln = f.readline().strip()
+                logging.info("Manifest line read: %s", ln)
+                sln = ln.split(DELIM)
+                if len(ln) < 3:
+                    logging.warning(
+                        "Unexpected data format: Split line has length: %s, expected length of 4. Line was: %s",
+                        len(sln),
+                        ln,
+                    )
+                    continue
 
-            try:
-                gdc.download_file(sln[I_ID], sln[I_MD5_SUM], sln[I_FILENAME])
-            except DownloadError as e:
-                logging.warning("Download failed with reason: %s", e.reason)
-                download_error_handler(ln, e.reason)
+                try:
+                    out_path = OUT_DIR_PATH + "/" +  sln[I_FILENAME]
+                    gdc.download_file(sln[I_ID], sln[I_MD5_SUM], out_path)
+                    p.write(sln[I_ID] + "\t" + out_path + "\t" + sln[I_MD5_SUM] + "\n")
+                except DownloadError as e:
+                    logging.warning("Download failed with reason: %s", e.reason)
+                    download_error_handler(ln, e.reason)
