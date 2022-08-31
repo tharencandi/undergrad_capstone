8c463d76673cac7ffdfefe8fc2ebb01a4a8d227d
jmic8397 <jmic8397@uni.sydney.edu.au>
2022-08-31 18:46:20 +1000

    watershed produces low accuracy output mask

--- src/watershed.py
+++ src/watershed.py
@@ -5,55 +5,95 @@ from matplotlib import pyplot as plt
 from skimage import measure, color, io
 from skimage.segmentation import clear_border
 
+DEBUG = False
+
+#To be confirmed with scale of original image
+#pixels_to_um = 0.454
+
 # load in image
-img = cv.imread("image01.png")
+img = cv.imread("image01_without_mask.png")
 
 # cells = img[:,:,0]
 # plt.imshow(cells, cmap = "gray")
 
+#Try blur image before grey?
+img_blur = cv.medianBlur(img,7)
+
 # create gray image and find threshold value
 # note: threshold pixels will be set to 255
-gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-r1, threshold = cv.threshold(gray_img, 0, 255, cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
+image01GrayBlur = cv.cvtColor(img_blur, cv.COLOR_BGR2GRAY)
+
+#Whats gray blur look like
+if(DEBUG):cv.imshow("image01_gray_blur",image01GrayBlur)
+
+#Save grey blur img
+plt.imsave("image01_gray_blur.png",image01GrayBlur)
+
+#filter blue channel?
+image01GrayBlur = cv.imread("image01_gray_blur.png")
+cells = image01GrayBlur[:,:,0]
+
+#What does filtered look like?
+if(DEBUG): cv.imshow("gray_filtered_img",cells)
+
+r1, threshold = cv.threshold(cells, 0, 255, 
+cv.THRESH_BINARY +cv.THRESH_OTSU)
 
 # remove small initial noise from image
-kernel = np.ones((3,3), np.unit8)
-opening = cv.morphologyEx(threshold, cv.MORPH_OPEN, kernel, iterations = 2)
+kernel = np.ones((3,3), np.uint8)
+opening = cv.morphologyEx(threshold, cv.MORPH_OPEN, 
+kernel, iterations = 2)
 
-# remove edge touching grains
+#What does opening look like?
+if(DEBUG): cv.imshow("opening_pre_clear",opening)
+
+# remove edge touching grains (This removed everything? - removed)
 # opening = clear_border(opening)
-# plt.imshow(opening, cmap = "gray")
+# cv.imshow("opening_post_clear",opening)
 
-# watershed for area that is sure background
-sure_bg = cv.dilate(opening, kernel, iteration = 10)
-# plt.imshow(sure_bg, cmap = "gray")
+# Define sure background
+sure_bg = cv.dilate(opening, kernel, iterations = 3)
 
-# watershed for area that is sure foreground
+# What does sure background look like?
+if(DEBUG): cv.imshow("sure_bg",sure_bg)
+
+# transform for area that is sure foreground
 dist_transform = cv.distanceTransform(opening, cv.DIST_L2, 5)
-# plt.imshow(dist_transform, cmap = "gray")
 max_dist_transform = dist_transform.max()
-r2, sure_fg = cv.threshold(dist_transform, 0.5*max_dist_transform, 255, 0)
-# plt.imshow(sure_fg, cmap = "gray")
+r2, sure_fg = cv.threshold(dist_transform, 
+0.05*max_dist_transform, 255, 0)
+
+# What does sure foreground look like?
+if(DEBUG): cv.imshow("sure_fg",sure_fg)
 
 # getting unknown ambiguous region (background - foreground)
-sure_fg = np.unit8(sure_fg)
+sure_fg = np.uint8(sure_fg)
 unknown = cv.subtract(sure_bg, sure_fg)
-# plt.imshow(unknown, cmap = "gray")
 
 # Create marker and label the regions
 r3, markers = cv.connectedComponents(sure_fg)
-# plt.imshow(markers)
+
+# What does markers look like?
+# if(DEBUG): cv.imshow("markers",markers)
+
+#Set marker vectors fg != 0
 markers = markers + 10
+#Set bg = 0
 markers[unknown == 255] = 0
-# plt.imshow(markers, cmap = "jet")
+
+#perform watershed
 markers = cv.watershed(img, markers)
-img[markers == -1] = [0, 255, 255]
+
+#reassign boundaries
+img[markers == -1] = [0,255,0]
 
 # return an RBG image where color-coded labels are painted over
-img2 = color.label2rgb(markers, bg_label = 0)
+Coloured_watershed_run_on_greyblur = color.label2rgb(markers, bg_label = 0)
 
-# display images
-plt.imshow(img2)
-plt.imshow("Overlay", img)
-plt.imshow("Color Grains", img2)
+# save images
+plt.imsave("image01_attempt.png",img)
+
+
+# plt.imshow("Overlay", img2)
+# plt.imshow("Color Grains", img2)
 cv.waitKey(0)
