359e88d487d4e918a30353f196583d6322a03db6
Tharen Candi <tcan7909@uni.sydney.edu.au>
2022-09-07 18:44:07 +0200

    svs experimentation

--- src/CNN/predict.py
+++ src/CNN/predict.py
@@ -3,6 +3,8 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import cnn_model as cnn_model
+import openslide as osl
+import openslide.deepzoom
 from skimage.util.shape import view_as_windows 
 IMG_SIZE = (102, 102)
 MODEL = 'data/models/model_76'
@@ -45,16 +47,44 @@ def predict_image(img):
     return img_mask
 
 
+def predict_validation_set():
+    for i in range(1,2):
+    
+        img = cv.imread("data/validation/image{:02d}.png".format(i))
+        mask = predict_image(img)
+        mask= cv.cvtColor(np.float32(mask),cv.COLOR_GRAY2RGB)
+        true_mask =  cv.imread("data/validation/image{:02d}_mask.png".format(i))
+        vis = np.concatenate((img, true_mask, mask), axis=1)
+        #vis = cv.resize(vis, (vis.shape[1], vis.shape[0]*4))
+        cv.imshow("sds", vis)
+        cv.waitKey()
+        cv.imwrite('data/results/out_combined.png', vis)
 
-for i in range(1,2):
-   
-    img = cv.imread("data/validation/image{:02d}.png".format(i))
-    mask = predict_image(img)
-    mask= cv.cvtColor(np.float32(mask),cv.COLOR_GRAY2RGB)
-    true_mask =  cv.imread("data/validation/image{:02d}_mask.png".format(i))
-    vis = np.concatenate((img, true_mask, mask), axis=1)
-    #vis = cv.resize(vis, (vis.shape[1], vis.shape[0]*4))
-    cv.imshow("sds", vis)
+
+def predict_slide(svs_file, location):
+
+    slide = osl.OpenSlide(svs_file)
+    size = (int(slide.properties["aperio.OriginalHeight"]), int(slide.properties["aperio.OriginalWidth"]))
+    print(size)
+    size_0 = (int(slide.properties['openslide.level[0].height']),int(slide.properties['openslide.level[0].width']))
+    tile_0_size = (int(slide.properties['openslide.level[0].tile-height']), int(slide.properties['openslide.level[0].tile-width']))
+    
+    dz = osl.deepzoom.DeepZoomGenerator(slide, tile_size=254, overlap=1, limit_bounds=False)
+    print(dz.level_dimensions)
+    tile = dz.get_tile(0, (0,0))
+    tile = np.array(tile)
+    cv.imshow("tile", tile)
     cv.waitKey()
-    cv.imwrite('data/results/out_combined.png', vis)
- 
+    slide.close()
+    exit(0)
+    region_size = (size[0]//zoom, size[1]//zoom)
+
+    for i in range(zoom):
+        for j in range(zoom):
+
+            img = slide.read_region((i*region_size[1], j*region_size[0] ), 0, region_size )
+            cv_image = np.array(img) 
+            cv.imwrite(f"{location}/{i}_ {j}.jpg", cv_image)
+
+    slide.close()
+
