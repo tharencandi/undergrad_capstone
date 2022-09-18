1792cc91b3a88eb7c55bfd51d068c0b144fb17d0
H <haeata.ash@pm.me>
2022-09-18 12:54:50 +1000

    Added server skeleton and mount script

--- src/management_app/app.py
+++ src/management_app/app.py
@@ -1,9 +1,24 @@
+
 from flask import Flask
 
 
+app = Flask(__name__)
 
+@app.post("/<string:case>/<uuid>/svs")
+def receive_svs(case, uuid):
+    print("svs")
+    pass
 
+@app.post("/<string:case>/<uuid>/mask")
+def receive_mask(case, uuid):
+    print("mask")
+    pass
+
+@app.post("/<string:case>/<uuid>/annotation")
+def receive_annotation(case, uuid):
+    print("annotation")
+    pass
 
 if __name__=="__main__":
-    app = Flask(__name__)
+
     app.run()
---
+++ src/management_app/hardrive_mount.sh
@@ -0,0 +1,22 @@
+MOUNTPOINT="/home/haeata/.glibiostoma_data"
+DEVICE="/dev/$(lsblk -fs | grep -e "ntfs" | grep -e "Passport" | cut -f1 -d" ")"
+
+if [[ -e $MOUNTPOINT  && -d $MOUNTPOINT ]]; then
+	echo $MOUNTPOINT exists
+else
+	echo $MOUNTPOINT does not exist
+	echo Creating directory $MOUNTPOINT
+	mkdir $MOUNTPOINT
+fi
+
+echo "Device: $DEVICE, Mountpoint: $MOUNTPOINT"
+echo -n "Proceed [y/n]: "
+read -r CONFIRM
+
+if [[ $CONFIRM == "y" ]]; then
+	echo "Mounting device..."
+	sudo mount -t ntfs3 $DEVICE $MOUNTPOINT
+else
+	echo "aborting..."
+fi
+
