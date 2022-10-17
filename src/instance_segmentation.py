de5cc1d420d2f27f05c4cf9942b50b4913fd7214
Gorjan <gmur3456@uni.sydney.edu.au>
2022-10-17 17:08:32 +1100

    update instance segmentation

--- src/instance_segmentation.py
+++ src/instance_segmentation.py
@@ -97,3 +97,34 @@ print(y_train_cat.shape)
 print(y_test_cat.shape)
 
 
+# Reused parameters in all models
+n_classes=3
+activation='softmax'
+
+LR = 0.0001
+optim = keras.optimizers.Adam(LR)
+
+# Segmentation models losses can be combined together by '+' and scaled by integer or float factor
+# set class weights for dice_loss (car: 1.; pedestrian: 2.; background: 0.5;)
+dice_loss = sm.losses.DiceLoss(class_weights=np.array([0.25, 0.25, 0.25])) 
+focal_loss = sm.losses.CategoricalFocalLoss()
+total_loss = dice_loss + (1 * focal_loss)
+
+# actulally total_loss can be imported directly from library, above example just show you how to manipulate with losses
+# total_loss = sm.losses.binary_focal_dice_loss # or sm.losses.categorical_focal_dice_loss 
+
+metrics = [sm.metrics.IOUScore(threshold=0.5), sm.metrics.FScore(threshold=0.5)]
+
+###Model 1
+BACKBONE1 = 'resnet34'
+preprocess_input1 = sm.get_preprocessing(BACKBONE1)
+
+# preprocess input
+X_train1 = preprocess_input1(X_train)
+X_test1 = preprocess_input1(X_test)
+
+# define model
+model1 = sm.Unet(BACKBONE1, encoder_weights='imagenet', classes=n_classes, activation=activation)
+
+# compile keras model with defined optimozer, loss and metrics
+model1.compile(optim, total_loss, metrics=metrics)
