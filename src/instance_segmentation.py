9140e80f1684be787911301a015b13e2e94932a4
Gorjan <gmur3456@uni.sydney.edu.au>
2022-10-17 17:07:17 +1100

    update instance segmentation

--- src/instance_segmentation.py
+++ src/instance_segmentation.py
@@ -42,9 +42,35 @@ for directory_path in glob.glob("data/generated_patches/images/"):
         else:
           break
         count+=1
-
+        
 #Convert list to array for machine learning processing        
 train_images = np.array(train_images)
 print(train_images.shape)
 
 
+#Capture training image info as a list
+train_masks = [] 
+
+for directory_path in glob.glob("data/generated_patches/masks_with_border/"):
+    count = 0
+    for mask_path in glob.glob(os.path.join(directory_path, "*.tif")):
+        if count < total_images_to_load:
+          my_mask_path=directory_path+"mask"+str(count)+".tif"
+          #print("Reading mask :", my_mask_path)
+          mask = cv2.imread(my_mask_path, 0)       
+          #mask = cv2.resize(mask, (SIZE_Y, SIZE_X), interpolation = cv2.INTER_NEAREST)  #Otherwise ground truth changes due to interpolation
+          train_masks.append(mask)
+        else:
+          break
+        count+=1
+#Convert list to array for machine learning processing        
+train_masks = np.array(train_masks)
+print(train_masks.shape)
+
+#Encode labels... but multi dim array so need to flatten, encode and reshape
+from sklearn.preprocessing import LabelEncoder
+labelencoder = LabelEncoder()
+n, h, w = train_masks.shape
+train_masks_reshaped = train_masks.reshape(-1,1)
+train_masks_reshaped_encoded = labelencoder.fit_transform(train_masks_reshaped)
+train_masks_encoded_original_shape = train_masks_reshaped_encoded.reshape(n, h, w)
