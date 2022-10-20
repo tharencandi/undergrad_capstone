236d8c13181669d655dcb96b15c873d783201f02
H <haeata.ash@pm.me>
2022-10-21 06:56:24 +1100

    removed uneeded sql functionality

--- src/webportal/app.py
+++ src/webportal/app.py
@@ -26,33 +26,33 @@ scan_path = "./scans/"
 valid_extensions = ["png", "svs", "tif"]
 
 WEB_PORTAL_DIR="/home/haeata/.glioblastoma_portal/"
-DATABASE = '/home/haeata/.glioblastoma_portal/file.db'
+# DATABASE = '/home/haeata/.glioblastoma_portal/file.db'
 
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
+# def query_db(query, args=(), one=False):
+#     cur = get_db().execute(query, args)
+#     rv = cur.fetchall()
+#     cur.close()
+#     return (rv[0] if rv else None) if one else rv
   
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-        db.row_factory = sqlite3.Row
-    return db
+# def get_db():
+#     db = getattr(g, '_database', None)
+#     if db is None:
+#         db = g._database = sqlite3.connect(DATABASE)
+#         db.row_factory = sqlite3.Row
+#     return db
 
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+# @app.teardown_appcontext
+# def close_connection(exception):
+#     db = getattr(g, '_database', None)
+#     if db is not None:
+#         db.close()
 
-def init_db_if_not_exists():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read()) 
-        db.commit()
+# def init_db_if_not_exists():
+#     with app.app_context():
+#         db = get_db()
+#         with app.open_resource('schema.sql', mode='r') as f:
+#             db.cursor().executescript(f.read()) 
+#         db.commit()
 
 # home page
 @app.get('/')
@@ -282,16 +282,13 @@ TASK_MAP = {
     ".mask": make_mask
 }
 
-def make_fpath(svs, ext):
+def make_fpath(uuid, ext):
     return ""
 
-def get_fpath(svs):
+def get_fpath(uuid, ext):
     return  ""
 
-def get_meta(svs):
-    return ""
-
-def set_meta_field(svs, field, value):
+def set_meta_field(uuid, field, value):
     return ""
 # run algo, generate mask
 @app.post('/generate')
