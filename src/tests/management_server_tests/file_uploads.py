ba4ec6e09aa59d2f91847a9b9f42479cef474834
H <haeata.ash@pm.me>
2022-10-22 08:49:27 +1100

    begun tests

--- src/tests/management_server_tests/file_uploads.py
+++ src/tests/management_server_tests/file_uploads.py
@@ -1,15 +1,22 @@
 import pytest
+import os
+import shutil
 from management_app.app import app, DATA_DIR
 
+TMP_DIR="temp_mgmt_server"
 @pytest.fixture()
 def test_client():
-    app.config[DATA_DIR] = "temp"
+    os.mkdir(TMP_DIR)
+    app.config[DATA_DIR] = TMP_DIR
     testing_client = app.test_client()
     ctx = flask_app.app_context()
     ctx.push()
     yield testing_client
+    shutil.rmtree(TMP_DIR)
     ctx.pop()
 
 
-def @pytest.fixture()
+def test_basic_upload(test_client):
+
+
 
