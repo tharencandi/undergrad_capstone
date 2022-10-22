461e8e471bb4943692eb52093377620dcd825fe2
Alex Lin <alex.lin9991@gmail.com>
2022-10-22 11:41:43 +1100

    disabled file conversion import temporarily for testing

--- src/webportal/app.py
+++ src/webportal/app.py
@@ -11,7 +11,7 @@ import sys, os
 cdir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.dirname(cdir))
 
-from image_tools.conversion import svs_to_png, svs_to_tiff
+# from image_tools.conversion import svs_to_png, svs_to_tiff
 
 
 app = Flask(__name__, static_url_path='',
