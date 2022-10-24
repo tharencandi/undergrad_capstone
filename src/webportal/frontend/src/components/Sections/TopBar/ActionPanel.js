f962d0a79646b7c23eb8138397054abf7d10d642
Alex Lin <alex.lin9991@gmail.com>
2022-10-24 13:18:55 +1100

    updated cancel button to only show if there are things in progress

--- src/webportal/frontend/src/components/Sections/TopBar/ActionPanel.js
+++ src/webportal/frontend/src/components/Sections/TopBar/ActionPanel.js
@@ -6,10 +6,14 @@ import { useState } from "react";
 import useUploadSVS from "hooks/useUploadSVS";
 import { useSelector } from "react-redux";
 
+import { Tooltip } from "@mui/material";
+import axios from "axios";
+
 const ActionPanel = () => {
   // Modal variants - "download", "generate", "none" (none means no modal open)
   const [modalVariant, setModalVariant] = useState("none");
   const selectedData = useSelector((state) => state.selectedData);
+  const data = useSelector((state) => state.data);
 
   const [
     setUploadQueue,
@@ -18,6 +22,32 @@ const ActionPanel = () => {
     numberToUpload,
     uploadError,
   ] = useUploadSVS();
+
+  let workInProgress = false;
+
+  for (let entry in data) {
+    if (
+      data[entry].tifStatus !== "none" &&
+      data[entry].tifStatus !== "completed"
+    ) {
+      workInProgress = true;
+    }
+    if (
+      data[entry].pngStatus !== "none" &&
+      data[entry].pngStatus !== "completed"
+    ) {
+      workInProgress = true;
+    }
+    if (
+      data[entry].maskStatus !== "none" &&
+      data[entry].maskStatus !== "completed"
+    ) {
+      workInProgress = true;
+    }
+    if (numberToUpload !== 0) {
+      workInProgress = true;
+    }
+  }
 
   const uploadFileChangeHandler = (e) => {
     setUploadQueue(e.target.files);
@@ -51,14 +81,8 @@ const ActionPanel = () => {
       >
         Delete
       </Button>
-      <Button
-        onClick={() => {
-          setModalVariant("cancel");
-        }}
-        danger
-        // TODO Run a function which detects any pending or in progress operations, and enables the button if detected
-      >
-        Cancel
+      <Button onClick={() => {}} danger hidden={!workInProgress}>
+        Cancel All
       </Button>
       {modalVariant === "none" ? null : (
         <Modal modalController={setModalVariant} variant={modalVariant}></Modal>
